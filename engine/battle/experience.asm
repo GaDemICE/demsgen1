985c1a6051b3184cca4495badbddb4ed636a2353
DemICE <93394429+GaDemICE@users.noreply.github.com>
2024-03-12 13:54:56 +0200

    Update experience.asm

--- engine/battle/experience.asm
+++ engine/battle/experience.asm
@@ -31,7 +31,7 @@ GainExperience:
 	ld a, [hli]
 	ld b, a ; enemy mon base stat
 	ld a, [de] ; stat exp
-	add b ; add enemy mon base state to stat exp
+	;add b ; add enemy mon base state to stat exp
 	ld [de], a
 	jr nc, .nextBaseStat
 ; if there was a carry, increment the upper byte
